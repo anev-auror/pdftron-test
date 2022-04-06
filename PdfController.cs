8cbda0bbb0c1c904c1c51be725047be55fc97f78
Angela Evans <angela.evans@auror.co>
2022-04-06 16:14:50 +1200

    initial commit

---
+++ PdfController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PdfTronTestService
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PdfController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "yes it works";
+        }
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using pdftron;
+
+namespace PdfTronTestService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers()
+                .AddJsonOptions(o => { o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+            PDFNet.Initialize("demo:1648513787256:7bce20460300000000fd420f38305cc1c60f21517f4fa6b28ca73f66cd");
+        }
+    }
+}
